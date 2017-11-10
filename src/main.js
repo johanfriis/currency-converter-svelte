443cd4ac1ada48fd685aef3f2fd49504a21a6e3e
Johan Friis <johanfriis@gmail.com>
2017-11-10 19:17:02 +0100

    Support old and new method of calling `addCurrencyConverter`

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import CurrencyConverter from './CurrencyConverter.html';
 
-function addCurrencyConverter(options) {
+function addCurrencyConverter(...args) {
+  const options = args.length === 4 ? {
+    target: args[0],
+    amount: args[1],
+    from: 'GBP',
+    to: args[2],
+    label: args[3]
+  } : args[0];
+
   const currencyConverter = new CurrencyConverter({
     target: document.querySelector('.' + options.target),
     data: {
